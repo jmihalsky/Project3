2bbf59805f968355c47b34e3943a54f33f9dcf9d
jeff mihalsky <jmihalsky@gmail.com>
2019-01-30 19:03:24 -0800

    basic sequelize and passport

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const session = require("express-session");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require("./routes");
 
+var passport = require("./configuration/passport");
 // Express
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,6 +16,11 @@ if (process.env.NODE_ENV === "production") {
 
 // Routes
 app.use(routes);
+
+// Passport info
+app.use(session({secret: "keyboard cat", resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Add Sequelize
 
