bbd9aef5ff4a2dbeeae864459003ac78de58c36a
thirteen <41817587+maguirre13@users.noreply.github.com>
2019-02-08 18:54:53 -0800

    add resorts container

---
+++ client/src/components/Resorts/ResortsComponent.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export class ResortsContainer extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          backgroundColor: "rgba(51, 51, 51, 0.9)",
+          minHeight: "100%",
+          color: "white",
+          textShadow: "2px 2px 4px #000",
+          textAlign: "center"
+        }}
+      >
+        <p>RESORT INFO HERE</p>
+      </div>
+    );
+  }
+}
+
+export default ResortsContainer;
