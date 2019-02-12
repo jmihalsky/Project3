af17375b6e6f4c503324722838fad80cc1837179
thirteen <41817587+maguirre13@users.noreply.github.com>
2019-02-11 21:36:17 -0800

    fix all sizing

--- client/src/components/ResortList/index.js
+++ client/src/components/ResortList/index.js
@@ -3,18 +3,18 @@ import Container from "../Container";
 import Row from "../Row";
 import Col from "../Col";
 
-export default function HomeResList(props){
-    return(
-        <Container>
-            <Row>
-                <Col size="md-12">
-                    <div className="card text-center" style={{marginBottom: "20px"}}>
-                        <div className="card-header">
-                            <h5>{props.resort_name}</h5>
-                        </div>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    );
+export default function HomeResList(props) {
+  return (
+    <Container>
+      <Row>
+        <Col size="md-12">
+          <div className="card text-center" style={{ backgroundColor: "#333" }}>
+            <div className="card-header">
+              <h5>{props.resort_name}</h5>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
--- client/src/components/Resorts/index.js
+++ client/src/components/Resorts/index.js
@@ -3,40 +3,40 @@ import HomeResList from "../ResortList";
 import API from "../../utils/API";
 
 export class ResortsContainer extends Component {
-    componentDidMount(){
-        this.getResorts();
-    }
+  componentDidMount() {
+    this.getResorts();
+  }
 
-    state = {
-        ResList: []
-    }
+  state = {
+    ResList: []
+  };
 
-    getResorts = () => {
-        API.AllResorts().then(res => {
-            this.setState({ResList: res.data});
-        });
-    }
+  getResorts = () => {
+    API.AllResorts().then(res => {
+      this.setState({ ResList: res.data });
+    });
+  };
 
-    resortCard = () => {
-        let temp = this.state.ResList.map(r =>
-            <HomeResList key={r.resort_id} resort_name={r.resort_name}/>
-        )
-        return temp;
-    }
+  resortCard = () => {
+    let temp = this.state.ResList.map(r => (
+      <HomeResList key={r.resort_id} resort_name={r.resort_name} />
+    ));
+    return temp;
+  };
   render() {
     return (
       <div
         style={{
           backgroundColor: "rgba(51, 51, 51, 0.5)",
-          minHeight: "100%",
           color: "white",
           textShadow: "2px 2px 4px #000",
-          textAlign: "center"
+          textAlign: "center",
+          overflowY: "scroll",
+          height: "865px"
         }}
       >
-        <p>RESORT INFO HERE</p>
+        <h3>Ski Resorts:</h3>
         {this.resortCard()}
-
       </div>
     );
   }
