151cd6720ec2cf417ee56411afb8009eb71d03e8
thirteen <41817587+maguirre13@users.noreply.github.com>
2019-02-16 10:08:04 -0800

    add jumbotron to resort page

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 
 export class Jumbotron extends Component {
   state = {
