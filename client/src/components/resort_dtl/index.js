60fe75fc42ed16e49196b4313b8c16f4d9e7e341
jeff mihalsky <jmihalsky@gmail.com>
2019-02-20 18:38:42 -0800

    resort page additions

--- client/src/components/resort_dtl/index.js
+++ client/src/components/resort_dtl/index.js
@@ -12,6 +12,8 @@ export default function ResortDtl(props){
                     <p>{props.address1}</p>
                     <p>{props.address2}</p>
                     <p>{props.city} {props.state},{props.postal_code}</p>
+                    <a href={props.web_link} target="_blank">Resort Website</a>
+                    <a href={props.map_link} target="_blank"><img src={props.map_link}></img></a>
                 </Col>
             </Row>
         </div>
